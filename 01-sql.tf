6814f3cd9cda77d67104e72ba756a16f8265808d
Magnus Jensen <magnushjensen.mail@gmail.com>
2023-11-02 22:00:12 +0100

    deploy sql and pass sql to cloud run backend

---
+++ 01-sql.tf
@@ -0,0 +1,33 @@
+resource "random_password" "root_password" {
+  length = 16
+  special = true
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = "europe-north1"
+  database_version = "MYSQL_8_0"
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      authorized_networks {
+        value = "0.0.0.0/0" // Allow all connections, but should be restricted to the VPC/network and only allow cloud run once that is provisioned.
+      }
+    }
+  }
+
+  deletion_protection  = "true"
+}
+
+resource "google_sql_user" "root_user" {
+  instance = google_sql_database_instance.instance.name
+  name = "root"
+  password = random_password.root_password.result
+}
