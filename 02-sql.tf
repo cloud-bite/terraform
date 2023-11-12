2037b951c8d9c3777c1107c77a67bbfb6ef519fe
Magnus Jensen <magnushjensen.mail@gmail.com>
2023-11-12 15:14:54 +0100

    create secure connection between sql and cloud run backend

--- 02-sql.tf
+++ 02-sql.tf
@@ -1,3 +1,29 @@
+resource "google_project_service" "servicenetworking-api" {
+  project = var.gcp_project
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_compute_network" "private_network" {
+  name = "private-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+  name = "private-ip-address"
+  purpose = "VPC_PEERING"
+  address_type = "INTERNAL"
+  prefix_length = 16
+  network = google_compute_network.private_network.id
+}
+
+resource "google_service_networking_connection" "default" {
+  network = google_compute_network.private_network.id
+  service = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+
+
 resource "random_password" "root_password" {
   length = 16
   special = true
@@ -14,12 +40,13 @@ resource "google_sql_database_instance" "instance" {
   region           = var.gcp_region
   database_version = "MYSQL_8_0"
 
+  depends_on = [ google_service_networking_connection.default ]
+
   settings {
     tier = "db-f1-micro"
     ip_configuration {
-      authorized_networks {
-        value = "0.0.0.0/0" // Allow all connections, but should be restricted to the VPC/network and only allow cloud run once that is provisioned.
-      }
+      ipv4_enabled = false
+      private_network = google_compute_network.private_network.id
     }
   }
 
@@ -30,4 +57,11 @@ resource "google_sql_user" "root_user" {
   instance = google_sql_database_instance.instance.name
   name = "root"
   password = random_password.root_password.result
+}
+
+resource "google_compute_network_peering_routes_config" "peering_routes" {
+  peering              = google_service_networking_connection.default.peering
+  network              = google_compute_network.private_network.name
+  import_custom_routes = true
+  export_custom_routes = true
 }
--- 03-backend.tf
+++ 03-backend.tf
@@ -1,3 +1,14 @@
+resource "google_project_service" "vpcaccess-api" {
+  project = var.gcp_project
+  service = "vpcaccess.googleapis.com"
+}
+
+resource "google_vpc_access_connector" "connector" {
+  name = "vpc-connector"
+  ip_cidr_range = "10.10.11.0/28"
+  network = google_compute_network.private_network.name
+}
+
 resource "google_cloud_run_v2_service" "backend" {
   name = "backend"
   location = var.gcp_region
@@ -9,6 +20,29 @@ resource "google_cloud_run_v2_service" "backend" {
       ports {
         container_port = 3000
       }
+
+      env {
+        name = "DB_HOST"
+        value = google_sql_database_instance.instance.private_ip_address # This is currently using the public IP; but should be transferred to the private when using the isolated VPC/network.
+      }
+      env {
+        name = "DB_NAME"
+        value = google_sql_database.database.name
+      }
+      env {
+        name = "DB_USER"
+        value = "root"
+      }
+      env {
+        name = "DB_PASS"
+        value = random_password.root_password.result
+      }
+    }
+
+    
+    vpc_access {
+      connector = google_vpc_access_connector.connector.id
+      egress = "ALL_TRAFFIC"
     }
   }
 }
