ee3cac974e581f62772bb33e0d433681623339b3
Magnus Jensen <magnushjensen.mail@gmail.com>
2023-11-12 14:07:32 +0100

    don't pass environment variables

--- 03-backend.tf
+++ 03-backend.tf
@@ -9,24 +9,6 @@ resource "google_cloud_run_v2_service" "backend" {
       ports {
         container_port = 3000
       }
-      
-      # These values are here, before they are set in secret manager.
-      env {
-        name = "DB_HOST"
-        value = google_sql_database_instance.instance.public_ip_address # This is currently using the public IP; but should be transferred to the private when using the isolated VPC/network.
-      }
-      env {
-        name = "DB_NAME"
-        value = google_sql_database.database.name
-      }
-      env {
-        name = "DB_USER"
-        value = "root"
-      }
-      env {
-        name = "DB_PASS"
-        value = random_password.root_password.result
-      }
     }
   }
 }
