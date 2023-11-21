c28e69c8577c93cacf97ecd59597fe4ae6de3650
Magnus Jensen <magnushjensen.mail@gmail.com>
2023-11-21 19:09:54 +0100

    add working frontend cdn

--- 04-frontend-bucket.tf
+++ 04-frontend-bucket.tf
@@ -1,6 +1,11 @@
 resource "google_storage_bucket" "website" {
   name = "cb-website"
   location = "EU"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page = "index.html"
+  }
 }
 
 resource "google_storage_default_object_access_control" "website_read" {
--- 05-frontend-cdn.tf
+++ 05-frontend-cdn.tf
@@ -2,20 +2,6 @@ resource "google_compute_global_address" "website" {
   name = "website-lb-ip"
 }
 
-resource "google_dns_managed_zone" "gcp_cb" {
-  name = "gcp-cb"
-  dns_name = "gcp-cb.com."
-  
-}
-
-resource "google_dns_record_set" "website" {
-  name = "website.${google_dns_managed_zone.gcp_cb.dns_name}"
-  type = "A"
-  ttl = 300
-  managed_zone = google_dns_managed_zone.gcp_cb.name
-  rrdatas = [google_compute_global_address.website.address]
-}
-
 resource "google_compute_backend_bucket" "website" {
   name = "website-backend"
   description = "Contains files needed by the website"
@@ -23,22 +9,14 @@ resource "google_compute_backend_bucket" "website" {
   enable_cdn = true
 }
 
-resource "google_compute_managed_ssl_certificate" "website" {
-  name = "website-cert"
-  managed {
-    domains = [google_dns_record_set.website.name]
-  }
-}
-
 resource "google_compute_url_map" "website" {
   name = "website-url-map"
-  default_service = google_compute_backend_bucket.website.self_link
+  default_service = google_compute_backend_bucket.website.id
 }
 
-resource "google_compute_target_https_proxy" "website" {
+resource "google_compute_target_http_proxy" "website" {
   name = "website-target-proxy"
-  url_map = google_compute_url_map.website.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.website.self_link]
+  url_map = google_compute_url_map.website.id
 }
 
 resource "google_compute_global_forwarding_rule" "default" {
@@ -46,6 +24,6 @@ resource "google_compute_global_forwarding_rule" "default" {
   load_balancing_scheme = "EXTERNAL"
   ip_address = google_compute_global_address.website.address
   ip_protocol = "TCP"
-  port_range = "443"
-  target = google_compute_target_https_proxy.website.self_link
+  port_range = "80"
+  target = google_compute_target_http_proxy.website.id
 }
